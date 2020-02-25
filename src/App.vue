5aae20c3603cc3927b9402635120f5eaca37f426
张志楠 <597825040@qq.com>
2020-02-25 21:02:40 +0800

    function init
    iOS 方法测试
    
    Co-Authored-By: zhinzhang <datouzxin@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,141 @@
 <template>
   <div id="app">
     <div id="nav">
-      <button @click="clickBtn">点击确认</button>
+      <div>
+        <button @click="clickSetStorage">本地存储</button>
+      </div>
+      <div>
+        <button @click="clickGetStorage">获取本地存储值</button>
+        <span>{{name}}</span>
+      </div>
+      <div>
+        <button @click="clickShowAlert">Alert显示</button>
+      </div>
+      <div>
+        <button @click="clickShowHudMessage">HUD显示消息</button>
+      </div>
+      <div>
+        <button @click="clickShowHudload">HUD加载中</button>
+      </div>
+      <div>
+        <button @click="clickHideHud">HUD隐藏</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
   methods: {
-    clickBtn() {
+    clickSetStorage() {
       let data = {
-        method: "getAppstorage"
+        method: "setAppStorage",
+        param: {
+          key: "student",
+          value: {
+            id: "js001",
+            name: "ming"
+          }
+        }
       };
+      this.postMessageFunction(data);
+    },
+    clickGetStorage() {
+      let data = {
+        method: "getAppstorage",
+        param: {
+          key: "student"
+        }
+      };
+      this.postMessageFunction(data);
+    },
+    returnAppStorage(key, value) {
+      console.log(value);
+
+      let student = JSON.parse(value);
+      alert(student.name);
+      this.name = student.name;
+    },
+    clickShowAlert() {
+      let data = {
+        method: "showAlertView",
+        param: {
+          title: "提示", //弹窗标题
+          message: "", //消息内容
+          actions: [
+            //弹窗数组 一个为alert样式,两个及以上为actionsheet样式
+            {
+              title: "按钮1", //弹窗标题
+              type: "0", //
+              is_default: "Y" // Y为UIAlertActionStyleDefault/N为UIAlertActionStyleDestructive
+            },
+            {
+              title: "按钮2",
+              type: "1",
+              is_default: "Y"
+            },
+            {
+              title: "删除",
+              type: "2",
+              is_default: "N"
+            },
+            {
+              title: "确认",
+              type: "cancle",
+              is_default: "Y"
+            }
+          ]
+        }
+      };
+      this.postMessageFunction(data);
+    },
+    clickShowHudMessage() {
+      let data = {
+        method: "performLoading",
+        param: {
+          type: "message",
+          message: "你好"
+        }
+      };
+      this.postMessageFunction(data);
+    },
+    clickShowHudload() {
+      let data = {
+        method: "performLoading",
+        param: {
+          type: "load",
+          message: "你好",
+          canClick: "N"
+        }
+      };
+      this.postMessageFunction(data);
+    },
+    clickHideHud() {
+      let data = {
+        method: "performLoading",
+        param: {
+          type: "hide"
+        }
+      };
+      this.postMessageFunction(data);
+    },
+    postMessageFunction(data) {
       window.webkit.messageHandlers.callAppMethod.postMessage(
         JSON.stringify(data)
       );
+    },
+    alertType(params) {
+      console.log(params);
     }
+  },
+  mounted() {
+    window.alertType = this.alertType;
+    window.returnAppStorage = this.returnAppStorage;
   }
 };
 </script>
